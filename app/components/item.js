13ae40b87f9e91cb3636b331e84bfe17cd325f73
Joan R <jrbellido@gmail.com>
2016-06-07 18:14:23 +0200

    Trying to print item names on list

--- app/components/item.js
+++ app/components/item.js
@@ -1,19 +1,14 @@
 import React, { PropTypes } from "react";
+import Immutable from "immutable"
 
 export default class Item extends React.Component {
-  static propTypes = {
-  	item: PropTypes.shape({
-  		id: PropTypes.number,
-  		name: PropTypes.string,
-  		value: PropTypes.number
-  	}).isRequired
-  }
-
   render() {
   	const item = this.props.item
 
+  	console.log(item.name[0])
+
     return (
-      <li className="item"><span>Item</span> <button>edit</button></li>
+      <li className="item"><span>{item.name}</span> <button>edit</button></li>
     )
   }
 }
--- app/reducers/ItemReducer.js
+++ app/reducers/ItemReducer.js
@@ -5,7 +5,8 @@ const defaultState = new Immutable.List()
 export default function ItemReducer(state = defaultState, action) {
   switch(action.type) {
     case 'GET_ITEMS':
-      return new Immutable.List(action.res.data)
+      const res = new Immutable.List(action.res.data)
+      return res
     case 'CREATE_ITEM':
       return state.concat(action.name, action.value)
     case 'EDIT_ITEM':
