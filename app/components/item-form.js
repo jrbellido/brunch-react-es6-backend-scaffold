84903aedd077e217af1684e36c8d979cd10a9609
Joan R <jrbellido@gmail.com>
2016-06-11 12:51:53 +0200

    Clear form when submit

--- app/components/item-form.js
+++ app/components/item-form.js
@@ -18,7 +18,10 @@ export default class ItemForm extends Component {
   	console.dump("ItemForm->handleSubmit", this, ev)
 
     const form = this.refs.formElement
-    this.props.createItem(form.name.value, form.value.value)
+    this.props.createItem(form.name.value, form.value.value).then(() => {
+      form.name.value = ""
+      form.value.value = ""
+    })
 
   	ev.preventDefault()
   }
@@ -27,8 +30,8 @@ export default class ItemForm extends Component {
     return (
       <form className="item-form" onSubmit={(e) => this.handleSubmit(e)} ref="formElement">
         <h3>Submit item</h3>
-        <input name="name" type="text" />
-        <input name="value" type="text" />
+        <input name="name" placeholder="Name" type="text" />
+        <input name="value" placeholder="Value" type="text" />
         <button>Submit</button>
       </form>
     )
