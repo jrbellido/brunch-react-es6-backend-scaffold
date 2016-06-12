dddf89fd20f3a304d69d8889be550a4df94097f2
Joan R <jrbellido@gmail.com>
2016-06-12 20:02:42 +0200

    Remove logging

--- app/components/about.js
+++ app/components/about.js
@@ -1,18 +1,6 @@
 import React, { PropTypes } from "react"
 
 export default class About extends React.Component {
-  componentWillMount() { console.log("About->componentWillMount") }
-
-  componentDidMount() { console.log("About->componentDidMount") }
-
-  componentWillReceiveProps() { console.log("About->componentWillReceiveProps") }
-
-  componentWillUpdate() { console.log("About->componentWillUpdate") }
-
-  componentDidUpdate() { console.log("About->componentDidUpdate") }
-
-  componentWillUnmount() { console.log("About->componentWillUnmount") }
-
   render() {
     console.dump("About->render", this)
 
